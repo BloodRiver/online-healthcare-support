1d18b8023464fcb342a1fee08fd16d89bfc0d5f0
BloodRiver <sag.arnob@gmail.com>
2026-05-18 06:29:15 +0000

    fixed settings bug

--- django_website/settings.py
+++ django_website/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tw=_^xc6q!=69*vyw=1m0bgcq&_c1qxw+pm7v-&vf(a7mkg^g2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sagacity.pythonanywhere.com']
 
 
 # Application definition
